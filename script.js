38857579fd2bf3938d4b4943f76b110cef59ef8b
Samantha Simmons <sammysimmons274@gmail.com>
2020-02-02 17:10:29 +0000

    commit

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
- 
-
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
-	});
-	$("#stream2_btn").on("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream2").addClass('highlight_stream');
-	});
-	$("#stream3_btn").on("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream3").addClass('highlight_stream');
-	});
 
 
-}); 
+    $("#stream1_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+    });
+    $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+    });
+    $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+    });
+
+    $("p").click(function() {
+        $("p").css('color', 'red');
+    });
+
+    $("h2").hover(function() {
+        $("h2").css('background-color', 'lightblue');
+    });
+
+    $
+
+});
