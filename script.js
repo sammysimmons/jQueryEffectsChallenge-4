b04b6ef8274819176def38531104938118321c93
Samantha Simmons <sammysimmons274@gmail.com>
2020-02-07 13:52:35 +0000

    adding files to git

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 $(document).ready(function() {
 
 
-    $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
-    });
-    $("#stream2_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream2").addClass('highlight_stream');
-    });
-    $("#stream3_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream3").addClass('highlight_stream');
-    });
+            $("#stream1_btn").on("click", function() {
+                $(".stream1").removeClass('highlight_stream');
+                $(".stream2").removeClass('highlight_stream');
+                $(".stream3").removeClass('highlight_stream');
+                $(".stream1").addClass('highlight_stream');
+            });
+            $("#stream2_btn").on("click", function() {
+                $(".stream1").removeClass('highlight_stream');
+                $(".stream2").removeClass('highlight_stream');
+                $(".stream3").removeClass('highlight_stream');
+                $(".stream2").addClass('highlight_stream');
+            });
+            $("#stream3_btn").on("click", function() {
+                $(".stream1").removeClass('highlight_stream');
+                $(".stream2").removeClass('highlight_stream');
+                $(".stream3").removeClass('highlight_stream');
+                $(".stream3").addClass('highlight_stream');
+            });
 
-    $("p").click(function() {
-        $("p").css('color', 'red');
-    });
+            $("p") on.click(function() {
+                $("p").css('color', 'red');
+            });
 
-    $("h2").hover(function() {
-        $("h2").css('background-color', 'lightblue');
-    });
+            $("h2").hover(function() {
+                $("h2").css('background-color', 'lightblue');
+            });
 
-    $
+            $(document).ready(function() {
+                        $("#button_effects1").click(function() {
+                            $('#button_effects1').hide('slow');
 
-});
+                        });
